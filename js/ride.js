c6953b8f3b11148a50066ac6e69b92810eddd59d
codereyes-1 <80291124+codereyes-1@users.noreply.github.com>
2023-04-21 16:59:47 -0700

    background color

--- js/ride.js
+++ js/ride.js
@@ -86,7 +86,6 @@ WildRydes.map = WildRydes.map || {};
         var dest = WildRydes.map.selectedPoint;
         var origin = {};
         var speed = 1600;
-        
 
         if (dest.latitude > WildRydes.map.center.latitude) {
             origin.latitude = WildRydes.map.extent.minLat;
