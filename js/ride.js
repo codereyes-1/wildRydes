cfb743771880143868a59be81d3f071413ddd31c
codereyes-1 <80291124+codereyes-1@users.noreply.github.com>
2025-07-05 16:55:04 -0700

    readme

--- js/ride.js
+++ js/ride.js
@@ -218,7 +218,7 @@ WildRydes.map = WildRydes.map || {};
         }
 
         WildRydes.map.animate(origin, dest, speed, callback);
-    }
+    }``
 
     function displayUpdate(text) {
         $('#updates').append($('<li>' + text + '</li>'));
